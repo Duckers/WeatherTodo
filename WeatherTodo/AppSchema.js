2d8bfd997b07c678bd87d7666bbb9140849f51ce
Lars Thomas Denstad <lars.denstad@knowit.no>
2017-03-15 16:05:18 +0100

    This adds addTodo to the schema

--- WeatherTodo/AppSchema.js
+++ WeatherTodo/AppSchema.js
@@ -12,6 +12,10 @@ function AppSchema() {
 		"id", String, "The ID of the todo",
 		"isDone", Boolean, "Whether or not the todo item is done");
 
+	this.addTodo = Schema.method("Adds a todo to the todolist",
+		"title", String, "The title of the TODO item",
+		"description", String, "What am I supposed to do, exactly?",
+		"preferredWeather", String, "Under which weather conditions would I rather do this?");
 }
 
 module.exports = AppSchema;
