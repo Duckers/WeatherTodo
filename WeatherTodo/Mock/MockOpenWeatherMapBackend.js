24ea4670c32a726b53c7de9a17cf0db198fac703
Lars Thomas Denstad <lars.denstad@knowit.no>
2017-03-15 16:05:00 +0100

    This adds a list of temperatures to the mock

--- WeatherTodo/Mock/MockOpenWeatherMapBackend.js
+++ WeatherTodo/Mock/MockOpenWeatherMapBackend.js
@@ -2,10 +2,21 @@
 function MockOpenWeatherMapBackend() {
 	
 	this.weather = function(next, obs, city) {
-		obs.value = {
-			temperature: 40,
-			symbol: "sunny"
-		}
+		obs.addAll(
+			[
+				from: "2017-03-03T06:00:00",
+				to: "2017-03-03T09:00:00",
+				temperature: 40,
+				weather: "sunny"
+			],
+			[
+				from: "2017-03-03T09:00:00",
+				to: "2017-03-03T12:00:00",
+				temperature: 12,
+				weather: "raining"	
+			]
+		});
+		return obs;
 	}
 
 }
