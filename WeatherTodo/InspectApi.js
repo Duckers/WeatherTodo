64f7c7f86af44147b1eb4291d804e4dff071dad6
Lars Thomas Denstad <cocporn@gmail.com>
2017-03-20 17:51:19 +0100

    This fixes a bug in InspectApi

--- WeatherTodo/InspectApi.js
+++ WeatherTodo/InspectApi.js
@@ -5,15 +5,18 @@ function InspectApi() {
 	var self = this;
 	var args = Array.prototype.slice.call(arguments);
 
+	function createListener(symbol) {		
+		var m = self.fabric[symbol];				
+		if (m instanceof Observable) {
+			m.onValueChanged(module, function (item) {
+				self.fabric.debug(symbol + ' onValueChanged: ' + JSON.stringify(item));
+			});				
+		}
+	}
+
 	this.create = function() {
 		for (var i = 0; i < args.length; i++) {	
-			var symbol = args[i];
-			var m = self.fabric[symbol];				
-			if (m instanceof Observable) {
-				m.onValueChanged(module, function (item) {
-					self.fabric.debug(symbol + ' onValueChanged: ' + JSON.stringify(item));
-				});				
-			}
+			createListener(args[i]);
 		}
 	}
 
