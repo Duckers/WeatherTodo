ea1e3a010f7c460bfaa2b329c2f6f1ede3c0f73e
Lars Thomas Denstad <cocporn@gmail.com>
2017-03-21 13:12:25 +0100

    This implements the sorting and crude displaying of the data

--- WeatherTodo/AppData.js
+++ WeatherTodo/AppData.js
@@ -21,19 +21,88 @@ function AppData() {
 			});
 	});
 
+	var doneTodos = todos.filter(function (t) { return t.isDone === true; });
+
+	var notDoneTodos = todos.filter(function (t) { return t.isDone === false });
+
 	var forecast = Forecast.list(function (latitude, longitude) {
 		this.fabric.debug('Refreshing forecast');
 		return this.next.fetchForecast(latitude, longitude);
 	});
 
+	var days = [
+		"Sunday",
+		"Monday",
+		"Tuesday",
+		"Wednesday",
+		"Thursday",
+		"Friday",
+		"Saturday"
+	];
+
+	// 
 	var sortedTodos = todos.combineArrays(forecast, function (todos, forecast) {
 		console.log('Created combined array: ' + JSON.stringify(todos));
-		return todos;
+
+		var todoList = [];
+
+		// Map todos to:
+		// 1. Incomplete todos
+		var todosToBeDone = todos.filter(function(t) { return t.isDone === false; });
+
+		// 2. Done todos
+		var doneTodos = todos.filter(function(t) { return t.isDone === true; });
+
+		// 3. Map incomplete todos to their corresponding weather
+		var weatherTypes = {};
+
+		todosToBeDone.forEach(function(t)  {
+			if (t.preferredWeather in weatherTypes === false) {
+				weatherTypes[t.preferredWeather] = [];
+			}
+			weatherTypes[t.preferredWeather].push(t);
+		});
+
+		// Map todos on forecast
+		forecast.forEach(function(f) {
+			//console.log(JSON.stringify(f));
+			if (f.weather in weatherTypes) {
+				weatherTypes[f.weather].forEach(function (t) {
+					t.fromTime = f.time;
+					t.toTime = new Date(f.time.getTime() + (3 * 60 * 60 * 1000));
+					t.timespan = days[t.fromTime.getDay()] + ' ' + t.fromTime.getHours() + ':00 - ' + t.toTime.getHours() + ':00';
+					t.day = days[t.fromTime.getDay()];
+					t.fromHour = t.fromTime.getHours() + ':00';
+					t.toHour = t.toTime.getHours() + ':00';
+					t.forecasted = true;
+					todoList.push(t);
+				});
+				delete weatherTypes[f.weather];
+			}
+		});
+
+		// Add remaining todos
+		for (key in weatherTypes) {
+			weatherTypes[key].forEach(function (t) {
+				t.forecasted = false;
+				todoList.push(t);
+			});
+		}
+
+		//todosToBeDone.forEach(function(t) { todoList.push(t); });
+		doneTodos.forEach(function(t) { 
+			t.forecasted = false;
+			todoList.push(t); 
+		});
+
+		return todoList;
 	});
 
 	this.todos = todos;
 	this.forecast = forecast;
 	this.sortedTodos = sortedTodos;
+
+	this.getTodo = Todo.get;
 
 	this.currentWeather = CurrentWeather.item(function (latitude, longitude) {
 		this.fabric.debug('Refreshing currentWeather');	
