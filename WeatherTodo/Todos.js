49f1b88bb765a2331c76f6f61bb42962d6a1ed50
Lars Thomas Denstad <cocporn@gmail.com>
2017-03-29 12:36:35 +0200

    This fixes issue #5 by calculating the day label

--- WeatherTodo/Todos.js
+++ WeatherTodo/Todos.js
@@ -72,7 +72,7 @@ function Todos() {
 			// Get the weather types that match the current weather (not forecasted)
 			if (currentWeather.weather in weatherTypes) {
 				weatherTypes[currentWeather.weather].forEach(function (t) {
-					t.timespan = 'NOW';
+					t.timespan = 'Now';
 					t.weatherAssigned = true;
 					todoList.push(t);
 				});
@@ -84,12 +84,19 @@ function Todos() {
 				// Because the store will implicitly serialize the data when stored,
 				// the information about f.time actually being a Date-instance is lost,
 				// so we need to reconstruct that
+				debugger;
 				f.time = new Date(f.time);
+				var todayDay = new Date().getDay();
 				if (f.weather in weatherTypes) {
 					weatherTypes[f.weather].forEach(function (t) {
+						var dayLabel = 'Today ';
+						if (todayDay !== f.time.getDay()) {
+							dayLabel = days[f.time.getDay()] + ' ';
+						}
+
 						t.fromTime = f.time;
 						t.toTime = new Date(f.time.getTime() + (3 * 60 * 60 * 1000));
-						t.timespan = days[t.fromTime.getDay()] + ' ' + t.fromTime.getHours() + ':00 - ' + t.toTime.getHours() + ':00';
+						t.timespan = dayLabel + t.fromTime.getHours() + ':00 - ' + t.toTime.getHours() + ':00';
 						t.day = days[t.fromTime.getDay()];
 						t.fromHour = t.fromTime.getHours() + ':00';
 						t.toHour = t.toTime.getHours() + ':00';
